f864eddb758efbf44844db59c6b598d5f0ab1652
Eric Passmore <eric.passmore@gmail.com>
2024-09-23 12:34:35 -0700

    silly config is not genesis.json

--- run-load-benchmark.sh
+++ run-load-benchmark.sh
@@ -7,13 +7,13 @@ cd /home/enf-replay/benchmark-nodeos || exit
 # get the commit hash if we want to see exact version later
 COMMIT_HASH=$(git rev-parse HEAD)
 cd /home/enf-replay || exit
-./benchmark-nodeos/monitor_script.sh $TITLE $(basename $CONFIG)-${COMMIT_HASH}&
+./benchmark-nodeos/monitor_script.sh $TITLE $(basename $CONFIG)-${COMMIT_HASH} &
 MONITOR_PID=$!
 
 [ -f /data/nodeos.log ] && :> /data/nodeos.log
 nodeos --config $CONFIG \
 --data-dir /data \
---genesis-json /home/enf-replay/benchmark-nodeos/config/heap-mode.ini \
+--genesis-json /home/enf-replay/benchmark-nodeos/config/genesis.json \
 --terminate-at-block 10000 > /data/nodeos.log
 
 set +x
