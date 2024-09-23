b3537762fed05dc9de09d77499b33963759cbf55
Eric Passmore <eric.passmore@gmail.com>
2024-09-23 11:19:08 -0700

    fix nodoes log file redirect

--- run-load-benchmark.sh
+++ run-load-benchmark.sh
@@ -6,7 +6,7 @@ MONITOR_PID=$$
 nodeos --config /home/enf-replay/config/heap-mode.ini \
 --data-dir /data \
 --genesis-json /home/enf-replay/config/genesis.json \
---terminate-at-block 10000 2>&1 /data/nodeos.log
+--terminate-at-block 10000 2>&1 > /data/nodeos.log
 
 set +x
 sleep 10
