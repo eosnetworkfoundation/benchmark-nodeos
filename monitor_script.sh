2e10265f549845c99a9ac536c1ad41bd87abb717
Eric Passmore <eric.passmore@gmail.com>
2024-10-03 08:48:28 -0700

    50 line lookback on logs when calc stages

--- monitor_script.sh
+++ monitor_script.sh
@@ -40,34 +40,34 @@ while true; do
   nodeos_info="NA"
   db_size_info="NA"
   # snapshot
-  IF_SNAP=$(tail -100 /data/nodeos.log | grep "Snapshot initialization" | wc -l)
+  IF_SNAP=$(tail -50 /data/nodeos.log | grep "Snapshot initialization" | wc -l)
   if [ "$IF_SNAP" -gt 0 ]; then
     stage="loading-snap"
   fi
   # chainbase read
-  IF_CHAINBASE_LOAD=$(tail -100 /data/nodeos.log | grep 'CHAINBASE: Preloading "state" database' | wc -l)
+  IF_CHAINBASE_LOAD=$(tail -50 /data/nodeos.log | grep 'CHAINBASE: Preloading "state" database' | wc -l)
   if [ "$IF_CHAINBASE_LOAD" -gt 0 ]; then
     stage="chainbase-load"
   fi
   # catch up via blocks log
-  IF_REPLAY=$(tail -100 /data/nodeos.log | grep "replay_block_log" | wc -l)
+  IF_REPLAY=$(tail -50 /data/nodeos.log | grep "replay_block_log" | wc -l)
   if [ "$IF_REPLAY" -gt 0 ]; then
     stage="replay"
   fi
   # catch up via peer sync
-  IF_SYNC=$(tail -100 /data/nodeos.log | grep "Sending handshake generation" | wc -l)
+  IF_SYNC=$(tail -50 /data/nodeos.log | grep "Sending handshake generation" | wc -l)
   if [ "$IF_SYNC" -gt 0 ]; then
     stage="net-sync"
     nodeos_info=$(curl http://127.0.0.1:8888/v1/chain/get_info | cut -d',' -f3,4) 2> /dev/null
     db_size_info=$(curl http://127.0.0.1:8888/v1/db_size/get | xargs | cut -d',' -f1-4) 2> /dev/null
   fi
   # reach max block, don't need to include termination
-  IF_END=$(tail -500 /data/nodeos.log | grep "reached configured maximum block" | wc -l)
+  IF_END=$(tail -50 /data/nodeos.log | grep "reached configured maximum block" | wc -l)
   if [ "$IF_END" -gt 0 ]; then
     stage="terminating"
   fi
   # chainbase write
-  IF_CHAINBASE_PERSIST=$(tail -100 /data/nodeos.log | grep 'CHAINBASE: Writing "state" database' | wc -l)
+  IF_CHAINBASE_PERSIST=$(tail -50 /data/nodeos.log | grep 'CHAINBASE: Writing "state" database' | wc -l)
   if [ "$IF_CHAINBASE_PERSIST" -gt 0 ]; then
     stage="chainbase-persist"
   fi
