40e266cc0be98e666e8240cfca20ed02ae152233
Eric Passmore <eric.passmore@gmail.com>
2024-10-04 10:45:20 -0700

    bug fix and single file option

--- process-data/process-stats.py
+++ process-data/process-stats.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import re
 import json
+from datetime import datetime
 
 class Benchmark:
     def __init__(self):
@@ -38,25 +39,49 @@ class Benchmark:
     def remove(self,key):
         del self.data[key]
 
-    def csv(self,key):
-        contents = f"{self.data[key]["stats"][0].csv_header()},{self.data[key]["host"].csv_header()}\n"
+    def csv_header(self):
+        # get first key
+        key = next(iter(self.data.keys()))
+        return f"{self.data[key]["stats"][0].csv_header()},{self.data[key]["host"].csv_header()}\n"
+
+    def csv(self,key,no_header=False):
+        contents = ""
+        # ok to print header?
+        if not no_header:
+            contents = self.csv_header()
+
         host_data = self.data[key]["host"].csv()
 
         for stats in self.data[key]["stats"]:
             contents = f"{contents}{stats.csv()},{host_data}\n"
         return contents
 
-    def json(self, key):
+    def json(self, key,is_array=False):
+        host_padding = 1
+        stats_padding = 2
         contents = '{\n'
+        closing = '}\n'
+        if is_array:
+            host_padding = 2
+            stats_padding = 3
+            contents = '    {\n'
+            closing = '    },\n'
         # host data indent level one
-        contents += self.data[key]["host"].json(1)
+        contents += self.data[key]["host"].json(host_padding)
         # build up stats as array
-        contents += '    [\n'
+        if is_array:
+            contents += '        [\n'
+        else:
+            contents += '    [\n'
         for stat_data in self.data[key]["stats"]:
-            contents += stat_data.json(2)
-        contents += '    ],\n'
+            contents += stat_data.json(stats_padding)
+        if is_array:
+            contents += '        ],\n'
+        else:
+            contents += '    ],\n'
+
         # close
-        contents += '}\n'
+        contents += closing
         return contents
 
 class Host():
@@ -114,13 +139,21 @@ class Host():
         )
 
 class Stats():
-    def __init__(self, date, stage, r, b, swpd, free, buff, cache, si, so, bi, bo, in_, cs, us, sy, id_, wa, st):
+    time_format = "%Y-%m-%dT%H:%M:%SZ"
+    def __init__(self, start_date, date, stage, r, b, swpd, free, buff, cache, si, so, bi, bo, in_, cs, us, sy, id_, wa, st, head_block_num, db_rows):
 
         if "T" in date:
             [self.date,self.time]  = date.split("T")   # Date/Time when the stats were collected
         else:
             self.date = date
             self.time = "Unknown"
+
+        self.elasped_secs = 0
+        if start_date:
+            dt1 = datetime.strptime(start_date, Stats.time_format)
+            dt2 = datetime.strptime(date, Stats.time_format)
+            # Calculate the difference in seconds
+            self.elasped_secs = (dt2 - dt1).total_seconds()
 
         self.stage = stage          # Stage (can be a label like "stage1", "stage2", etc.)
 
@@ -153,34 +186,41 @@ class Stats():
         self.wa = wa                # CPU time spent waiting for IO (%)
         self.st = st                # CPU time stolen by hypervisor (%)
 
+        # Nodeos Info
+        self.head_block_num = head_block_num    # head block for chain
+        self.db_rows = db_rows                  # db rows in chainbase
+
     def __str__(self):
-        return (f"Date: {self.date}, Time: {self.time}, Stage: {self.stage}\n"
+        return (f"Date: {self.date}, Time: {self.time}, Elasped_Sec: {self.elasped_secs}, Stage: {self.stage}\n"
                 f"Procs - r: {self.r}, b: {self.b}\n"
                 f"Memory - swpd: {self.swpd}, free: {self.free}, buff: {self.buff}, cache: {self.cache}\n"
                 f"Swap - si: {self.si}, so: {self.so}\n"
                 f"IO - bi: {self.bi}, bo: {self.bo}\n"
                 f"System - in: {self.in_}, cs: {self.cs}\n"
-                f"CPU - us: {self.us}, sy: {self.sy}, id: {self.id_}, wa: {self.wa}, st: {self.st}\n")
+                f"CPU - us: {self.us}, sy: {self.sy}, id: {self.id_}, wa: {self.wa}, st: {self.st}\n"
+                f"Nodeos - block: {self.head_block_num}, dbrows: {self.db_rows}\n")
 
     @staticmethod
     def csv_header():
-        headerInitial = "Date,Time,Stage,"
+        headerInitial = "Date,Time,Elasped_Sec,Stage,"
         headerProcs = "ProcsRunning,ProcsBlocked,"
         headerMemory = "Swapped(Kb),Free(Kb),Buffer(Kb),Cache(Kb),"
         headerSwap = "SwapIn(Kb/sec),SwapOut(Kb/sec),"
         headerIO = "BlocksIn(blocks/sec),BlocksOut(blocks/sec),"
         headerSys = "Interrupts,ContextSwitch,"
-        headerCPU = "CPU_User_Percent,CPU_Sys_Percent,CPU_Idle_Percent,CPU_WaitIO_Percent,CPU_Stolen_Percent"
+        headerCPU = "CPU_User_Percent,CPU_Sys_Percent,CPU_Idle_Percent,CPU_WaitIO_Percent,CPU_Stolen_Percent,"
+        nodeos_info = "Head_Block,DB_Rows"
         return headerInitial+headerProcs+headerMemory+headerSwap+headerIO+headerSys+headerCPU
 
     def csv(self):
-        return (f"{self.date},{self.time},{self.stage}"
+        return (f"{self.date},{self.time},{self.elasped_secs},{self.stage},"
                 f"{self.r},{self.b}"
-                f"{self.swpd},{self.free},{self.buff},{self.cache}"
-                f"{self.si},{self.so}"
-                f"{self.bi},{self.bo}"
-                f"{self.in_},{self.cs}"
-                f"{self.us},{self.sy},{self.id_},{self.wa},{self.st}")
+                f"{self.swpd},{self.free},{self.buff},{self.cache},"
+                f"{self.si},{self.so},"
+                f"{self.bi},{self.bo},"
+                f"{self.in_},{self.cs},"
+                f"{self.us},{self.sy},{self.id_},{self.wa},{self.st},"
+                f"{self.head_block_num},{self.db_rows}")
 
     def json(self, indent=0):
         indent=indent*4 # 4 spaces for each indent
@@ -192,6 +232,7 @@ class Stats():
         return(f'{padding}{{\n'
         f'    {padding}date:{self.date},\n'
         f'    {padding}time:{self.time},\n'
+        f'    {padding}elasped_secs:{self.elasped_secs},\n'
         f'    {padding}stage:{self.stage},\n'
         f'    {padding}procs_running:{self.r},\n'
         f'    {padding}procs_blocked:{self.b},\n'
@@ -210,6 +251,8 @@ class Stats():
         f'    {padding}cpu_idle_percent:{self.id_},\n'
         f'    {padding}cpu_waitio_percent:{self.wa},\n'
         f'    {padding}cpu_stolen_percent:{self.st}\n'
+        f'    {padding}head_block_num:{self.head_block_num},\n'
+        f'    {padding}db_rows:{self.db_rows}\n'
         f'{padding}}}{trailing_comma}\n'
         )
 
@@ -219,7 +262,7 @@ def parse_host_file(file_content):
     # A basic pattern-based parsing logic (modify as needed for actual file formats)
     title = re.search(r"^>>\s*(.*)", file_content)
     date = re.search(r"Date:\s*(.*)", file_content)
-    total_ram = re.search(r"Total RAM:\s*(.*)", file_content)
+    total_ram = re.search(r"Total [Rr][Aa][Mm]:\s*(.*)", file_content)
     total_swap = re.search(r"Total Swap:\s*(.*)", file_content)
     huge_pages = re.search(r"Number of 1Gb Pages:\s*(.*)", file_content)
     os_info = re.search(r"OS Info:\s*(.*)", file_content)
@@ -240,7 +283,7 @@ def parse_host_file(file_content):
         total_swap.group(1) if total_swap else "Unknown",
         huge_pages.group(1) if huge_pages else "Unknown",
         os_info.group(1) if os_info else "Unknown",
-        processor_model.group(1) if processor_model else "Unknown",
+        processor_model.group(1).replace(':','') if processor_model else "Unknown",
         number_cores.group(1) if number_cores else "Unknown",
         nodeos_version.group(1) if nodeos_version else "Unknown",
         config_file.group(1) if config_file else "Unknown"
@@ -249,15 +292,21 @@ def parse_host_file(file_content):
 def parse_stats_file(file_contents):
     all_lines = file_contents.split("\n")
     title_pattern = re.compile(r"^>>\s*(.*)")
+    earliest_line = True
 
     stats = []   # array of stats objects
+    start_date=None
     for line in all_lines:
         if title_pattern.match(line):
             continue     # skip line
         else:
             data = re.split(r'\s+', line)
             if len(data) >= 19:
+                if earliest_line:
+                    start_date = data[0]
+                    earliest_line = False
                 stat_obj = Stats(
+                    start_date, # first timestamp calc incr time
                     data[0], # date
                     data[1], # stage
                     data[2], # procs r
@@ -276,11 +325,11 @@ def parse_stats_file(file_contents):
                     data[15], # cpu sys
                     data[16], # cpu idle
                     data[17], # cpu wait io
-                    data[18] # cpu st
+                    data[18], # cpu st
+                    data[19], # head block num
+                    data[20]  # db rows
                 )
                 stats.append(stat_obj)
-            else:
-                print (f"Error: not enough data in {line}\n")
     return stats
 
 def parse_txt_files(input_directory):
@@ -288,8 +337,8 @@ def parse_txt_files(input_directory):
     benchmark = Benchmark()
 
     # Regex pattern to match filenames like host_123.txt stat_123.txt
-    is_host_file = re.compile(r"host_(\d+)\.txt")
-    is_stats_file = re.compile(r"stats_(\d+)\.txt")
+    is_host_file = re.compile(r"host[-_](\d+)\.txt")
+    is_stats_file = re.compile(r"stats[_-](\d+)\.txt")
 
     # List all files in the input directory
     for file_name in os.listdir(input_directory):
@@ -319,35 +368,55 @@ def parse_txt_files(input_directory):
                 benchmark.add_stats(file_number,stats)
     return benchmark
 
-def write_files(benchmark, output_directory, format):
+def write_files(benchmark, output_directory, format, single_file):
     # Check if output directory exists, if not, create it
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
-    for key in benchmark.keys():
-        file_name = f"{output_directory}/{benchmark.get_title(key)}"
+
+    # ouput to one file
+    if single_file:
         if format.lower() == "json":
-            file_name += ".json"
-            print (f"FILE: {file_name}")
+            file_name = f"{output_directory}/all.json"
+            print (f"Writing File: {file_name}")
             with open(file_name, "w") as file:
-                # Write the string to the file
-                file.write(benchmark.json(key))
+                file.write('[\n')
+                for key in benchmark.keys():
+                    file.write(benchmark.json(key,True))
+                file.write(']\n')
         else:
-            file_name += ".csv"
-            print (f"FILE: {file_name}")
+            file_name = f"{output_directory}/all.csv"
+            print (f"Writing File: {file_name}")
             with open(file_name, "w") as file:
-                # Write the string to the file
-                file.write(benchmark.csv(key))
+                file.write(benchmark.csv_header())
+                for key in benchmark.keys():
+                    file.write(benchmark.csv(key,True))
+    else:
+        for key in benchmark.keys():
+            file_name = f"{output_directory}/{benchmark.get_title(key)}"
+            if format.lower() == "json":
+                file_name += ".json"
+                print (f"Writing File: {file_name}")
+                with open(file_name, "w") as file:
+                    # Write the string to the file
+                    file.write(benchmark.json(key))
+            else:
+                file_name += ".csv"
+                print (f"Writing File: {file_name}")
+                with open(file_name, "w") as file:
+                    # Write the string to the file
+                    file.write(benchmark.csv(key))
 
 def main():
     parser = argparse.ArgumentParser(description="Parse stats from benchmarking")
     parser.add_argument('--input-directory', type=str, help='Path to the input directory')
     parser.add_argument('--output-directory', type=str, help='Path to the output directory')
     parser.add_argument('--format', type=str, default='CSV', help='CVS or JSON defaults to CSV')
+    parser.add_argument('--output-single-file', action='store_true', help='creates single output file')
 
     args = parser.parse_args()
 
     data = parse_txt_files(args.input_directory)
-    write_files(data, args.output_directory, args.format)
+    write_files(data, args.output_directory, args.format, args.output_single_file)
 
 if __name__ == "__main__":
     main()
